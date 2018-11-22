86e5f8816b47c434b2735f6ac1ffe3d61098ce14
Marsous <l.marcialis@gmail.com>
2018-11-22 08:51:46 +0100

    Travail sur exercices chapitre_5

---
+++ JS/javascript-web/chapitre_5/js/compteursClics.js
@@ -0,0 +1,23 @@
+/*
+1) Linker l'évènement clic bouton à la fonction incrémentation de clic
+*/
+var clicElt = document.getElementById("clic");
+clicElt.addEventListener("click", function () {
+	
+	}
+
+/*
+2) Créer la fonction incrémentation du clic sur le bouton
+	> Directement dans la foncton du clic
+3) Linker la fonction incrémentation de clic à l'id #clic
+	> Création de la variable en amont
+4) Linker l'évènement clic bouton à la fonction désactivation du comptage
+*/
+var desactiverElt = document.getElementById("desactiver");
+desactiverElt.addEventListener("click", function () {
+	
+	}
+/*
+5) Créer la fonction de désactivation du comptage
+6) Linker la fonction de désasctivation du comptage à l'id #desactiver
+*/
