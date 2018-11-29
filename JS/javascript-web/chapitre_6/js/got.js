40af2b5f603640cab92465834c94771d8bd5fe6a
Marsous <l.marcialis@gmail.com>
2018-11-29 20:37:59 +0100

    Exercice mot de passe chapitre_6

---
+++ JS/javascript-web/chapitre_6/js/got.js
@@ -0,0 +1,46 @@
+// Liste de quelques maisons de Game of Thrones. Chaque maison a un code et un nom
+var maisons = [
+    {
+        code: "ST",
+        nom: "Stark"
+    },
+    {
+        code: "LA",
+        nom: "Lannister"
+    },
+    {
+        code: "BA",
+        nom: "Baratheon"
+    },
+    {
+        code: "TA",
+        nom: "Targaryen"
+    }
+];
+
+// Renvoie un tableau contenant quelques personnages d'une maison
+function getPersonnages(codeMaison) {
+    switch (codeMaison) {
+    case "ST":
+        return ["Eddard", "Catelyn", "Robb", "Sansa", "Arya", "Jon Snow"];
+    case "LA":
+        return ["Tywin", "Cersei", "Jaime", "Tyrion"];
+    case "BA":
+        return ["Robert", "Stannis", "Renly"];
+    case "TA":
+        return ["Aerys", "Daenerys", "Viserys"];
+    default:
+        return [];
+    }
+};
+
+
+// Ajouter les maisons à la liste déroulante
+// Ajouter les personnages à la liste #persos
+// cChanger dynamiquement les personnages selon la maison sélectionnée
+
+maisons.forEach(function(maison) {
+    var maisonElt = document.createElement("option");
+    maisonElt.textContent = maison;
+    document.getElementById(maison).appendChild(maisonElt);
+})
