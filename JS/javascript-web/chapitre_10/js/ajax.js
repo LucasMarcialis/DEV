e74fecaf7ff6288f7bfa346c3e2826285f6f24d9
Marsous <l.marcialis@gmail.com>
2018-12-05 20:33:00 +0100

    Exercices chapitre_10

--- JS/javascript-web/chapitre_10/js/ajax.js
+++ JS/javascript-web/chapitre_10/js/ajax.js
@@ -1,3 +1,22 @@
+// Exécute un appel AJAX GET
+// Prend en paramètres l'URL cible et la fonction callback appelée en cas de succès
+function ajaxGet(url, callback) {
+    var req = new XMLHttpRequest();
+    req.open("GET", url);
+    req.addEventListener("load", function () {
+        if (req.status >= 200 && req.status < 400) {
+            // Appelle la fonction callback en lui passant la réponse de la requête
+            callback(req.responseText);
+        } else {
+            console.error(req.status + " " + req.statusText + " " + url);
+        }
+    });
+    req.addEventListener("error", function () {
+        console.error("Erreur réseau avec l'URL " + url);
+    });
+    req.send(null);
+}
+
 // Exécute un appel AJAX POST
 // Prend en paramètres l'URL cible, la donnée à envoyer et la fonction callback appelée en cas de succès
 // Le paramètre isJson permet d'indiquer si l'envoi concerne des données JSON
@@ -22,4 +41,4 @@ function ajaxPost(url, data, callback, isJson) {
         data = JSON.stringify(data);
     }
     req.send(data);
-}   
+}
